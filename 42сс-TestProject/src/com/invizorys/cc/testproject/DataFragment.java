b07676f5a5e3486d58f8a8a1803d8a975e5267de
Parishkura Roman <parishkura.roma@gmail.com>
2014-03-05 00:55:36 +0200

    ticket:3 Added edit_user_data dialog and custom DatePicker

--- 42сс-TestProject/src/com/invizorys/cc/testproject/DataFragment.java
+++ 42сс-TestProject/src/com/invizorys/cc/testproject/DataFragment.java
@@ -7,18 +7,29 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -28,6 +39,7 @@ import com.invizorys.cc.testproject.R;
 
 public class DataFragment extends SherlockFragment{
 	private TextView name, surname, birthday;
+	private Button buttonEditBirthday;
 	private ImageView userPhoto;
 	private DBHelper dbHelper;
 	private long userId;
@@ -48,6 +60,13 @@ public class DataFragment extends SherlockFragment{
 		birthday = (TextView) rootView.findViewById(R.id.textView_birthday);
 		userPhoto = (ImageView) rootView.findViewById(R.id.imageView_user_photo);
 		
+		rootView.findViewById(R.id.button_edit_data).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showEditDataDialog();
+			}
+		});
+		
 		dbHelper = new DBHelper(getActivity());
 		readDataFromBd();
 		
@@ -60,6 +79,97 @@ public class DataFragment extends SherlockFragment{
 		}
 		
 		return rootView;		
+	}
+	
+	private void showEditDataDialog() {
+		final Dialog dialog = new Dialog(getActivity());
+		dialog.setContentView(R.layout.edit_data_dialog);
+		dialog.setTitle("Edit data");
+
+		dialog.findViewById(R.id.button_ok).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//dialog.dismiss();
+			}
+		});
+		
+		dialog.findViewById(R.id.button_cancel).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();
+			}
+		});
+		
+		buttonEditBirthday = (Button) dialog.findViewById(R.id.button_edit_birthday);
+		buttonEditBirthday.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showDatePicker();
+			}
+		});
+		dialog.show();
+	}
+	
+	public void showDatePicker() {
+		LayoutInflater inflater = (LayoutInflater) getActivity().getLayoutInflater();
+		final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
+		View customView = inflater.inflate(R.layout.datepicker_layout, null);
+		dialogBuilder.setView(customView);
+		final Calendar now = Calendar.getInstance();
+		final DatePicker datePicker = (DatePicker) customView.findViewById(R.id.dialog_datepicker);
+		final TextView dateTextView = (TextView) customView.findViewById(R.id.dialog_dateview);
+		final SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy", Locale.UK);
+
+		dialogBuilder.setTitle("Choose a date");
+		Calendar choosenDate = Calendar.getInstance();
+		int year = choosenDate.get(Calendar.YEAR);
+		int month = choosenDate.get(Calendar.MONTH);
+		int day = choosenDate.get(Calendar.DAY_OF_MONTH);
+		try {
+			Date choosenDateFromUI = formatter.parse(buttonEditBirthday.getText().toString());
+			choosenDate.setTime(choosenDateFromUI);
+			year = choosenDate.get(Calendar.YEAR);
+			month = choosenDate.get(Calendar.MONTH);
+			day = choosenDate.get(Calendar.DAY_OF_MONTH);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Calendar dateToDisplay = Calendar.getInstance();
+		dateToDisplay.set(year, month, day);
+		dateTextView.setText(formatter.format(dateToDisplay.getTime()));
+
+		dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.dismiss();
+					}
+				});
+		dialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						Calendar choosen = Calendar.getInstance();
+						choosen.set(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth());
+						buttonEditBirthday.setText(formatter.format(choosen.getTime()));
+						dialog.dismiss();
+					}
+				});
+		final AlertDialog dialog = dialogBuilder.create();
+		datePicker.init(year, month, day,
+				new DatePicker.OnDateChangedListener() {
+					public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+						Calendar choosenDate = Calendar.getInstance();
+						choosenDate.set(year, monthOfYear, dayOfMonth);
+						dateTextView.setText(formatter.format(choosenDate.getTime()));
+						if (now.compareTo(choosenDate) < 0) {
+							dateTextView.setTextColor(Color.parseColor("#ff0000"));
+							((Button) dialog.getButton(AlertDialog.BUTTON_POSITIVE)).setEnabled(false);
+						} else {
+							dateTextView.setTextColor(Color.parseColor("#000000"));
+							((Button) dialog.getButton(AlertDialog.BUTTON_POSITIVE)).setEnabled(true);
+						}
+					}
+				});
+		dialog.show();
 	}
 
 	private void readDataFromBd() {
@@ -89,7 +199,7 @@ public class DataFragment extends SherlockFragment{
 	private void savePhotoOnSdcard(Bitmap bitmap) throws IOException
 	{
 		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-		bitmap.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+		bitmap.compress(Bitmap.CompressFormat.JPEG, 80, bytes);
 
 		new File(photoPath).mkdirs();
 		photoFile.createNewFile();
